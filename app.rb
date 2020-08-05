a5dfec36e297156490244d92c9a14c0d538788d4
TJBachorz <TJBachorz@gmail.com>
2020-08-05 10:47:50 -0600

    Completed all methods

--- app.rb
+++ app.rb
@@ -33,10 +33,21 @@ def companies_over_100(companies)
     (companies.filter { |company| company.size > 100})
 end 
 
-p companies_over_100(companies)  # <-- need to find a way to pull company.name as string
+#p companies_over_100(companies)  # <-- need to find a way to pull company.name as string
 
 def find_company_by_name(companies, name)
     (companies.find { |company| company.name == name })
 end
 
-puts find_company_by_name(companies, "Beta")
+#p find_company_by_name(companies, "Beta")
+
+def largest_company(companies)
+    companies.max_by {|company| company.size}
+end
+
+#p largest_company(companies)
+
+def largest_to_smallest_companies(companies)
+    companies.max_by(companies.length) { |company| company.size}
+end
+p largest_to_smallest_companies(companies)
