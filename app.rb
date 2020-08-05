9b1ee63676c4b3df9238d4eefc1b3047324dd781
TJBachorz <TJBachorz@gmail.com>
2020-08-05 16:59:05 -0600

    Fix for return values of methods

--- app.rb
+++ app.rb
@@ -17,6 +17,10 @@ companies = [
 
 ### Your code below this
 
+def company_as_string(company)
+    "#{company.name}" + " - " + "#{company.size}"
+end
+
 def company_names(companies)
     companies.collect { |company| company.name + " - " + company.size.to_s }
 end
@@ -30,24 +34,24 @@ end
 #p sum_sizes(companies)
 
 def companies_over_100(companies)
-    (companies.filter { |company| company.size > 100})
+    company_names(companies.filter { |company| company.size > 100})
 end 
 
-#p companies_over_100(companies)  # <-- need to find a way to pull company.name as string
+#p companies_over_100(companies) 
 
 def find_company_by_name(companies, name)
-    (companies.find { |company| company.name == name })
+    company_as_string(companies.find { |company| company.name == name })
 end
 
 #p find_company_by_name(companies, "Beta")
 
 def largest_company(companies)
-    companies.max_by {|company| company.size}
+    company_as_string(companies.max_by {|company| company.size})
 end
 
 #p largest_company(companies)
 
 def largest_to_smallest_companies(companies)
-    companies.max_by(companies.length) { |company| company.size}
+    company_names(companies.max_by(companies.length) { |company| company.size})
 end
-#p largest_to_smallest_companies(companies)
+p largest_to_smallest_companies(companies)
